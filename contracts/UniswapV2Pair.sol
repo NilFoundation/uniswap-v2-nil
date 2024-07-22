450888e075261e8f9535c79aaf83cfc99e14e22f
Aleksao998 <aleksaopacic988@gmail.com>
2024-07-22 20:22:36 +0200

    update blocktime

--- contracts/UniswapV2Pair.sol
+++ contracts/UniswapV2Pair.sol
@@ -93,8 +93,8 @@ contract UniswapV2Pair is NilBase, IUniswapV2Pair, UniswapV2ERC20 {
         //     balance0 <= type(uint112).max && balance1 <= type(uint112).max,
         //     "UniswapV2: OVERFLOW"
         // );
-        //uint32 blockTimestamp = uint32(block.timestamp % 2 ** 32);
-       // uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
+        uint32 blockTimestamp = uint32(block.timestamp % 2 ** 32);
+        //uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
         //if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {
             // // * never overflows, and + overflow is desired
             // price0CumulativeLast +=
@@ -107,7 +107,7 @@ contract UniswapV2Pair is NilBase, IUniswapV2Pair, UniswapV2ERC20 {
 
         reserve0 = balance0;
         reserve1 = balance1;
-       // blockTimestampLast = blockTimestamp;
+        blockTimestampLast = blockTimestamp;
 
        // emit Sync(reserve0, reserve1);
     }
