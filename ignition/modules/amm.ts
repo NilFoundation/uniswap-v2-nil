d866f63278c4e3a973ca154d588325f9ca6e6a8f
rolaman <rolamanbuss@yandex.ru>
2024-09-18 10:53:16 +0300

    nil-dex-with-router: add router contracts

--- ignition/modules/amm.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-module.exports = buildModule("DEX", (m: any) => {
-	const factory = m.contract("UniswapV2Factory", ["0x00019c49baa606A8781656C39136CbAfe73377c4".toLowerCase()]);
-	console.log("Deployed " + JSON.stringify(factory));
-	return { factory };
-});
